21f01f3225741cbe16f8eeeb1c7e64c23a67e799
Leonardo <leowitcroz@gmail.com>
2022-09-29 16:06:43 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+let nome = 'lafa'
+
+let nameArray = nome.split("");
+
+ 
+
+if(nameArray[0] == 'R' || nameArray[0] == 'r'){
+    console.log(`${nome}  sabe tocar banjo`)
+}else{
+
+    console.log(`${nome}  nao sabe tocar banjo`)
+}
+
+
+
+
+
+
+
+
+
+
